8324e7623fae7c6c08784b220dec2e8ed503578c
hpugym <983262246@qq.com>
2017-12-24 21:38:20 +0800

    增加 403

--- src/main/java/com/gym/exceptions/ExceptionController.java
+++ src/main/java/com/gym/exceptions/ExceptionController.java
@@ -4,7 +4,7 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 /**
- * 404、500异常处理
+ * 404、403、500异常处理
  * @author lemon
  *
  */
@@ -18,5 +18,10 @@ public class ExceptionController {
 	@RequestMapping("/error_404")
 	public String error_404() {
 		return "exceptions/error_404";
+	}
+	
+	@RequestMapping("/error_403")
+	public String error_403() {
+		return "exceptions/error_403";
 	}
 }
