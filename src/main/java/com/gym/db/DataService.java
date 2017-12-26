e3e7f3c9d8f52bb8c4c4e3e139d0d7d162736f49
hpugym <983262246@qq.com>
2017-12-26 21:15:59 +0800

    add jdbc

---
+++ src/main/java/com/gym/db/DataService.java
@@ -0,0 +1,71 @@
+package com.gym.db;
+
+import java.util.List;
+
+import javax.sql.DataSource;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.stereotype.Component;
+
+import net.sf.json.JSONArray;
+import net.sf.json.JSONObject;
+
+@Component
+public class DataService {
+	@Autowired
+	private DataSource dataSouce;
+	
+	/**
+	 * 加载jdbc
+	 * @return
+	 */
+	public JdbcTemplate getJdbcTemplate() {
+		return new JdbcTemplate(dataSouce);
+	}
+	
+	/**
+	 * 单条记录查询
+	 * @param sql
+	 * @param args
+	 * @return
+	 */
+	public JSONObject toJSONObject(String sql, Object ...args) {
+		JSONObject json = new JSONObject();
+		List<?> list = getJdbcTemplate().queryForList(sql, args);
+		if(list.size() == 0) {
+			return json;
+		}else {
+			return JSONObject.fromObject(list.get(0));
+		}
+	}
+	
+	/**
+	 * 多条数据查询
+	 * @param sql
+	 * @param args
+	 * @return
+	 */
+	public JSONArray toJSONArray(String sql, Object ...args) {
+		JSONArray jsonArray = new JSONArray();
+		List<?> list = getJdbcTemplate().queryForList(sql,args);
+		if(list.size() == 0) {
+			return jsonArray;
+		}else{
+			return JSONArray.fromObject(list);
+		}
+	}
+	
+	/**
+	 * 加载配置文件
+	 * @param tableName
+	 * @param name
+	 * @return
+	 */
+	public String getConfig(String tableName, String name) {
+		JSONObject json = new JSONObject();
+		json = toJSONObject("select value from "+tableName+" where name= "+name);
+		String returnName = json.optString(name,null);
+		return returnName;
+	}
+}
